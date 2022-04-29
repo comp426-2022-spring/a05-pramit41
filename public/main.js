7a6a792219e885106155317780eac558ddde3c0c
Pramit Thapa <pramit41@email.unc.edu>
2022-04-29 11:40:08 -0400

    finished with main.js

--- public/main.js
+++ public/main.js
@@ -144,6 +144,15 @@ async function sendFlips({url, formData}){
   return response.json()
 }
 
+function coinList(array) {
+  let text = "";
+  let arrayLength = array.length
+  for (let i = 0; i < arrayLength; i++) {
+    text += '<li><img src="assets/img/'+array[i]+'.png" class="bigcoin"></li>';
+  }
+  return text
+}
+
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
